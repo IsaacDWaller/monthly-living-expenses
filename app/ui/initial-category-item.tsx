fc2e458013b24014fbe82b3063e539a684e2ae90
Isaac Waller <isaac.d.waller@gmail.com>
2025-05-08 18:39:15 +1000

    feat: confirm delete category

--- app/ui/initial-category-item.tsx
+++ app/ui/initial-category-item.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 import { deleteCategory } from "@/app/lib/actions";
 import DeleteOutline from "@mui/icons-material/DeleteOutline";
 import EditOutlined from "@mui/icons-material/EditOutlined";
+import Dialog from "@mui/material/Dialog";
 import IconButton from "@mui/material/IconButton";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
+import { useState } from "react";
+import DialogTitle from "@mui/material/DialogTitle";
+import { Button, DialogActions, DialogContent, DialogContentText } from "@mui/material";
 
 type InitialCategoryItemProps = {
     name: string,
@@ -18,24 +24,59 @@ export default function InitialCategoryItem({
     numberOfExpenses,
     setBeingEdited,
 }: InitialCategoryItemProps) {
+    const [dialogIsOpen, setDialogIsOpen] = useState(false);
+
     const deleteCategoryWithName = deleteCategory.bind(null, name);
 
-    return <ListItem
-        secondaryAction={
-            <>
-                <IconButton onClick={() => setBeingEdited(true)}>
-                    <EditOutlined />
-                </IconButton >
+    function handleDeleteButtonClick() {
+        deleteCategoryWithName();
+        setDialogIsOpen(false);
+    }
 
-                <IconButton onClick={deleteCategoryWithName}>
-                    <DeleteOutline />
-                </IconButton>
-            </>
-        }
-    >
-        <ListItemText
-            primary={`${emoji} ${name}`}
-            secondary={`${numberOfExpenses} expenses`}
-        />
-    </ListItem>;
+    return <>
+        <Dialog
+            open={dialogIsOpen}
+            onClose={() => setDialogIsOpen(false)}
+            disableRestoreFocus
+        >
+            <DialogTitle>Delete category?</DialogTitle>
+
+            <DialogContent>
+                <DialogContentText>
+                    Confirm you want to delete {emoji} {name}
+                </DialogContentText>
+            </DialogContent>
+
+            <DialogActions>
+                <Button
+                    onClick={handleDeleteButtonClick}
+                    autoFocus
+                >
+                    Delete
+                </Button>
+
+                <Button onClick={() => setDialogIsOpen(false)}>Cancel</Button>
+            </DialogActions>
+            {/* onClick={deleteCategoryWithName} */}
+        </Dialog>
+
+        <ListItem
+            secondaryAction={
+                <>
+                    <IconButton onClick={() => setBeingEdited(true)}>
+                        <EditOutlined />
+                    </IconButton >
+
+                    <IconButton onClick={() => setDialogIsOpen(true)}>
+                        <DeleteOutline />
+                    </IconButton>
+                </>
+            }
+        >
+            <ListItemText
+                primary={`${emoji} ${name}`}
+                secondary={`${numberOfExpenses} expenses`}
+            />
+        </ListItem>
+    </>;
 }
